f75de43a50c44933f7503730ee2563e0be1931ff
Alex-mp4 <alexander.heino@elev.ga.ntig.se>
2023-02-09 13:10:02 +0100

    View init

--- src/Client/ClientView.java
+++ src/Client/ClientView.java
@@ -1,4 +1,11 @@
 package Client;
 
+import javax.swing.*;
+
 public class ClientView {
+    private JTextArea textArea1;
+    private JPanel panel1;
+    private JTextField textField1;
+    private JButton sendButton;
+    private JList list1;
 }
--- src/Server/ServerView.java
+++ src/Server/ServerView.java
@@ -1,4 +1,11 @@
 package Server;
 
+import javax.swing.*;
+
 public class ServerView {
+    private JTextArea textArea1;
+    private JPanel panel1;
+    private JTextField textField1;
+    private JButton sendButton;
+    private JList list1;
 }
